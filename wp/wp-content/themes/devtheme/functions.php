b5c510486eeb4f4c8067c30f2ff36cb536b4917e
Roind-hub <maskaremov@gmail.com>
2026-02-15 14:58:37 +0300

    register navigation menu

--- wp/wp-content/themes/devtheme/functions.php
+++ wp/wp-content/themes/devtheme/functions.php
@@ -12,3 +12,21 @@ function devtheme_enqueue_styles() {   //мы создаем функцию
 }
 
 add_action('wp_enqueue_scripts', 'devtheme_enqueue_styles'); //когда загружается страница → выполнить нашу функцию
+
+
+function devtheme_register_menus() {
+
+    // register_nav_menus — говорит WordPress:
+    // "в моей теме есть меню"
+    register_nav_menus(array(
+
+        // primary — это ID меню (используется в коде)
+        // Primary Menu — название в админке
+        'primary' => 'Primary Menu',
+
+    ));
+        
+    
+}
+
+add_action('after_setup_theme', 'devtheme_register_menus');
